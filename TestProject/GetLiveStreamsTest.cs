6db2b9ae9dff4030e31db3b4ecc5e0e0710d52cf
Xabier Jiménez Cuesta <xJimenezCuesta@gmail.com>
2025-06-14 15:40:52 +0200

    Feat add GetLiveStreamsTest

---
+++ TestProject/GetLiveStreamsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnica.Controllers;
+
+namespace TestProject
+{
+    public class GetLiveStreamsTest
+    {
+        private TwitchAPI _twitchAPI;
+
+        [SetUp]
+        public void Setup()
+        {
+            _twitchAPI = new TwitchAPI();
+        }
+
+        [Test]
+        public async Task Respuesta_200_cuando_llamada_correcta()
+        {
+            ObjectResult respuesta = await _twitchAPI.GetLiveStreams();
+            var algo = respuesta.Value.GetType();
+            Assert.AreEqual(respuesta.StatusCode, 200);
+        }
+    }
+}
